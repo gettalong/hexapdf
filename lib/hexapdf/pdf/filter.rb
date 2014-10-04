0a82d0b6b43ee3ed66da7921ecf51fbf90dc2737
Thomas Leitner <t_leitner@gmx.at>
2014-10-04 11:19:09 +0200

    Updated Filter documentation

--- lib/hexapdf/pdf/filter.rb
+++ lib/hexapdf/pdf/filter.rb
@@ -12,23 +12,24 @@ module HexaPDF
     # are specifically designed for image streams, like DCTDecode.
     #
     # Each filter is implemented via fibers. This allows HexaPDF to easily process either small
-    # chunks or a whole stream at once, depending on the memory restrictions.
+    # chunks or a whole stream at once, depending on the memory restrictions and to create flexible
+    # filter pipelines.
     #
     # It also allows the easy re-processing of a stream without first decoding and the encoding it.
     # Such functionality is useful, for example, when a PDF file should be decrypted and streams
     # compressed in one step.
     #
     #
-    # == Implementation of a Filter Module
+    # == Implementation of a Filter Object
     #
     # Each filter is an object (normally a module) that responds to two methods: #encoder and
     # #decoder. Both of these methods are given a *source* (a Fiber) and *options* (a Hash) and have
     # to return a Fiber object.
     #
-    # The returned fiber should resume the *source* fiber to get the next chunk of data (possibly
-    # only one byte of data, so this situation should be handled gracefully). Once the fiber has
-    # processed this chunk, it should yield the processed chunk as binary string. This should be
-    # done as long as the source fiber is #alive? and doesn't return +nil+ when resumed.
+    # The returned fiber should resume the *source* fiber to get the next chunk of binary data
+    # (possibly only one byte of data, so this situation should be handled gracefully). Once the
+    # fiber has processed this chunk, it should yield the processed chunk as binary string. This
+    # should be done as long as the source fiber is #alive? and doesn't return +nil+ when resumed.
     #
     # Such a fiber should *not* return +nil+ unless this signifies that no more data is coming!
     #
