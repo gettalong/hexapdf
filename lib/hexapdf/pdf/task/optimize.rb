75e6cf6fea7dc9b3ba76c21faf217cf8a75f9dc0
Thomas Leitner <t_leitner@gmx.at>
2015-04-25 08:56:29 +0200

    Updated optimization task, added compacting and object stream handling

--- lib/hexapdf/pdf/task/optimize.rb
+++ lib/hexapdf/pdf/task/optimize.rb
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8 -*-
 
+require 'set'
+
 module HexaPDF
   module PDF
     module Task
@@ -9,20 +11,108 @@ module HexaPDF
       # For a list of optimization methods this task can perform have a look at the ::call method.
       module Optimize
 
-        # Executes this task.
+        # Optimizes the PDF document.
         #
-        # The following optimization methods are performed if they are set to +true+:
+        # The field entries that are optional and set to their default value are always deleted.
+        # Additional optimization methods are performed depending on the values of the following
+        # arguments:
         #
-        # delete_fields_with_defaults::
-        #   Deletes field entries that are optional and set to their default value.
-        def self.call(doc, delete_fields_with_defaults: true)
-          doc.each(current: false) do |obj, rev|
-            next unless obj.kind_of?(HexaPDF::PDF::Dictionary)
-            obj.each do |name, value|
-              if (delete_fields_with_defaults && (field = obj.class.field(name)) &&
-                  !field.required? && field.default? && value == field.default)
-                obj.value.delete(name)
+        # compact::
+        #   Compacts the object space by merging the revisions and then deleting null and unused
+        #   values if set to +true+.
+        #
+        # object_streams::
+        #   Specifies if and how object streams should be used: For :preserve, existing object
+        #   streams are preserved; for :generate objects are packed into object streams as much as
+        #   possible; and for :delete existing object streams are deleted.
+        def self.call(doc, compact: false, object_streams: :preserve)
+          if compact
+            compact(doc, object_streams, &method(:delete_fields_with_defaults))
+          elsif object_streams != :preserve
+            process_object_streams(doc, object_streams, &method(:delete_fields_with_defaults))
+          else
+            doc.each(current: false, &method(:delete_fields_with_defaults))
+          end
+        end
+
+        # Compacts the document by merging all revisions into one, deleting null and unused entries
+        # and renumbering the objects if needed.
+        #
+        # For the meaning of the other arguments see ::call.
+        def self.compact(doc, object_streams)
+          doc.revisions.merge
+          unused = Set.new(doc.task(:dereference))
+          rev = doc.revisions.add
+
+          oid = 1
+          xref_stream = false
+          doc.revisions[0].each do |obj|
+            next if obj.empty? || unused.include?(obj) || (xref_stream && obj.type == :XRef) ||
+              (obj.type == :ObjStm && object_streams != :preserve)
+
+            xref_stream = true if obj.type == :XRef
+            yield(obj) if block_given?
+            obj.oid = oid
+            obj.gen = 0
+            rev.add(obj)
+            oid += 1
+          end
+          doc.revisions.delete(0)
+
+          process_object_streams(doc, :generate) if object_streams == :generate
+        end
+
+        # Processes the object streams in each revision according to method: For :preserve, nothing
+        # is done, for :disable all object streams are deleted and for :generate objects are packed
+        # into object streams as much as possible.
+        def self.process_object_streams(doc, method)
+          case method
+          when :preserve
+            # nothing to do
+          when :delete
+            doc.each(current: false) do |obj, rev|
+              if obj.type == :ObjStm
+                rev.delete(obj)
+              else
+                yield(obj) if block_given?
               end
+            end
+          when :generate
+            doc.revisions.each_with_index do |rev, rev_index|
+              xref_stream = false
+              count = 0
+              objstms = [doc.wrap({Type: :ObjStm})]
+              rev.each do |obj|
+                if obj.type == :XRef
+                  xref_stream = true
+                elsif obj.type == :ObjStm
+                  rev.delete(obj)
+                end
+                yield(obj) if block_given?
+
+                next if obj.respond_to?(:stream)
+
+                objstms[-1].add_object(obj)
+                count += 1
+                if count == 200
+                  objstms << doc.wrap({Type: :ObjStm})
+                  count = 0
+                end
+              end
+              objstms.each {|objstm| doc.add(objstm, revision: rev_index)}
+              doc.add({Type: :XRef}, revision: rev_index) unless xref_stream
+            end
+          end
+        end
+
+        # Deletes field entries of the object that are optional and currently set to their default
+        # value.
+        def self.delete_fields_with_defaults(obj)
+          return unless obj.kind_of?(HexaPDF::PDF::Dictionary)
+          obj.each do |name, value|
+            if (field = obj.class.field(name)) && !field.required? && field.default? &&
+                value == field.default
+              obj.delete(name)
             end
           end
         end
--- test/hexapdf/pdf/task/test_optimize.rb
+++ test/hexapdf/pdf/task/test_optimize.rb
@@ -11,13 +11,84 @@ describe HexaPDF::PDF::Task::Optimize do
   end
 
   before do
-    @doc = HexaPDF::PDF::Document.new
+    @doc = HexaPDF::PDF::Document.new(io: StringIO.new(MINIMAL_PDF))
     @obj = @doc.add(@doc.wrap({Optional: :Optional}, type: TestType))
+    @doc.trailer[:Test] = @obj
+    @doc.revisions.add
+    @obj1 = @doc.add({Type: :UsedEntry})
+    @obj2 = @doc.add({Unused: @obj1})
   end
 
-  it "deletes entries which are optional and set to their default value" do
-    @doc.task(:optimize, delete_fields_with_defaults: true)
+  def assert_objstms_generated
+    assert(@doc.revisions.all? {|rev| rev.any? {|obj| obj.type == :ObjStm}})
+    assert(@doc.revisions.all? {|rev| rev.any? {|obj| obj.type == :XRef}})
+  end
+
+  def assert_no_objstms
+    assert(@doc.each(current: false).all? {|obj| obj.type != :ObjStm})
+  end
+
+  def assert_default_deleted
     refute(@obj.value.key?(:Optional))
+  end
+
+  describe "compact" do
+    it "compacts the document" do
+      @doc.task(:optimize, compact: true)
+      refute(@doc.object?(@obj1))
+      refute(@doc.object?(@obj2))
+      assert_default_deleted
+      assert_equal(1, @doc.revisions.each.to_a.size)
+    end
+
+    it "compacts and generates object streams" do
+      @doc.task(:optimize, compact: true, object_streams: :generate)
+      assert_objstms_generated
+      assert_default_deleted
+      assert_default_deleted
+    end
+
+    it "compacts and deletes object streams" do
+      @doc.add({Type: :ObjStm})
+      @doc.task(:optimize, compact: true, object_streams: :delete)
+      assert_no_objstms
+      assert_default_deleted
+    end
+
+    it "compacts and preserves object streams" do
+      objstm = @doc.add({Type: :ObjStm})
+      @doc.task(:optimize, compact: true, object_streams: :preserve)
+      assert(@doc.object?(objstm))
+      assert_default_deleted
+    end
+  end
+
+  describe "object_streams" do
+    it "generates object streams" do
+      objstm = @doc.add({Type: :ObjStm})
+      xref = @doc.add({Type: :XRef})
+      210.times { @doc.add(5) }
+      @doc.task(:optimize, object_streams: :generate)
+      assert_objstms_generated
+      assert_default_deleted
+      assert_nil(@doc.object(objstm).value)
+      assert(3, @doc.revisions.current.find_all {|obj| obj.type == :ObjStm}.size)
+      assert([xref], @doc.revisions.current.find_all {|obj| obj.type == :XRef})
+    end
+
+    it "deletes object streams" do
+      @doc.add({Type: :ObjStm})
+      @doc.task(:optimize, object_streams: :delete)
+      assert_no_objstms
+      assert_default_deleted
+    end
+
+    it "preserves object streams" do
+      objstm = @doc.add({Type: :ObjStm})
+      @doc.task(:optimize, object_streams: :preserve)
+      assert(@doc.object?(objstm))
+      assert_default_deleted
+    end
   end
 
 end
