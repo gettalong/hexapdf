5b3a22e49a979cb92087aacffd75420f4fa81d10
Thomas Leitner <t_leitner@gmx.at>
2015-08-14 17:51:39 +0200

    Implemented Type::Resource#color_space method
    The method returns the color space implementation for a given name.

--- lib/hexapdf/pdf/type/resources.rb
+++ lib/hexapdf/pdf/type/resources.rb
@@ -1,6 +1,9 @@
 # -*- encoding: utf-8 -*-
 
+require 'hexapdf/error'
+require 'hexapdf/pdf/configuration'
 require 'hexapdf/pdf/dictionary'
+require 'hexapdf/pdf/content/color_space'
 
 module HexaPDF
   module PDF
@@ -21,6 +24,27 @@ module HexaPDF
         define_field :Properties, type: Dictionary, version: '1.2'
 
         define_validator(:validate_resources)
+
+        # Returns the color space stored under the given name.
+        #
+        # Note: The color spaces :DeviceGray, :DeviceRGB and :DeviceCMYK are returned without a
+        # lookup since they are fixed.
+        def color_space(name)
+          case name
+          when :DeviceRGB, :DeviceGray, :DeviceCMYK
+            GlobalConfiguration.constantize('color_space.map'.freeze, name).new
+          else
+            space_definition = self[:ColorSpace] && self[:ColorSpace][name]
+            if space_definition.nil?
+              raise HexaPDF::Error, "Color space '#{name}' not found in the resources"
+            end
+            space_name = (space_definition.kind_of?(Array) ? space_definition[0] : space_definition)
+
+            GlobalConfiguration.constantize('color_space.map'.freeze, space_name) do
+              Content::ColorSpace::Universal
+            end.new(space_definition)
+          end
+        end
 
         private
 
--- test/hexapdf/pdf/type/test_resources.rb
+++ test/hexapdf/pdf/type/test_resources.rb
@@ -2,20 +2,45 @@
 
 require 'test_helper'
 require 'hexapdf/pdf/type/resources'
+require 'hexapdf/pdf/document'
 
 describe HexaPDF::PDF::Type::Resources do
+  before do
+    doc = HexaPDF::PDF::Document.new
+    @res = HexaPDF::PDF::Type::Resources.new({}, document: doc)
+  end
+
+  describe "color_space" do
+    it "works for device color spaces" do
+      assert_equal(HexaPDF::PDF::Content::ColorSpace::DeviceRGB::DEFAULT,
+                   @res.color_space(:DeviceRGB))
+      assert_equal(HexaPDF::PDF::Content::ColorSpace::DeviceCMYK::DEFAULT,
+                   @res.color_space(:DeviceCMYK))
+      assert_equal(HexaPDF::PDF::Content::ColorSpace::DeviceGray::DEFAULT,
+                   @res.color_space(:DeviceGray))
+    end
+
+    it "returns the universal color space for unknown color space names" do
+      @res[:ColorSpace] = {CSName: [:SomeUnknownColorSpace, :some, :data, :here]}
+      assert_kind_of(HexaPDF::PDF::Content::ColorSpace::Universal,
+                     @res.color_space(:CSName))
+    end
+
+    it "fails if the specified name is neither a device color space nor in the dictionary" do
+      assert_raises(HexaPDF::Error) { @res.color_space(:UnknownColorSpace) }
+    end
+  end
+
   describe "validation" do
     it "assigns the default value if ProcSet is not set" do
-      res = HexaPDF::PDF::Type::Resources.new({})
-      res.validate
-      assert_equal([:PDF, :Text, :ImageB, :ImageC, :ImageI], res[:ProcSet])
+      @res.validate
+      assert_equal([:PDF, :Text, :ImageB, :ImageC, :ImageI], @res[:ProcSet])
     end
 
     it "removes invalid procedure set names from ProcSet" do
-      res = HexaPDF::PDF::Type::Resources.new({})
-      res[:ProcSet] = [:PDF, :Unknown]
-      res.validate
-      assert_equal([:PDF], res[:ProcSet])
+      @res[:ProcSet] = [:PDF, :Unknown]
+      @res.validate
+      assert_equal([:PDF], @res[:ProcSet])
     end
   end
 end
