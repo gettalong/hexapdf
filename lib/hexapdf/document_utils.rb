70db4d56d6f95177c2439057c834e84e8b586383
Thomas Leitner <t_leitner@gmx.at>
2016-03-22 17:57:49 +0100

    Added first implementation of file handler to the DocumentUtils class
    * Files and IO objects can be embedded, files can also just be
      referenced
    
    * All file specification dictionaries can be enumerated

--- lib/hexapdf/document_utils.rb
+++ lib/hexapdf/document_utils.rb
@@ -80,7 +80,93 @@ module HexaPDF
 
     end
 
+
+    # This module provides methods for managing file specification of a PDF file.
+    #
+    # Note that for a given PDF file not all file specifications may be found, e.g. when a file
+    # specification is only a string. Therefore this module can only handle those file
+    # specifications that are indirect file specification dictionaries with the /Type key set.
+    module Files
+
+      # :call-seq:
+      #   files.add_file(filename, name: File.basename(filename), description: nil, embed: true) -> file_spec
+      #   files.add_file(io, name:, description: nil)                      -> file_spec
+      #
+      # Adds the file or IO to the PDF and returns the corresponding file specification object.
+      #
+      # Options:
+      #
+      # name::
+      #     The name that should be used for the file path. This name is also for registering the
+      #     file in the EmbeddedFiles name tree.
+      #
+      # description::
+      #     A description of the file.
+      #
+      # embed::
+      #     When an IO object is given, it is always embedded and this option is ignored.
+      #
+      #     When a filename is given and this option is +true+, then the file is embedded. Otherwise
+      #     only a reference to it is stored.
+      #
+      # See: HexaPDF::Type::FileSpecification
+      def add_file(file_or_io, name: nil, description: nil, embed: true)
+        name ||= File.basename(file_or_io) if file_or_io.kind_of?(String)
+        if name.nil?
+          raise ArgumentError, "The name argument is mandatory when given an IO object"
+        end
+
+        spec = @document.add(Type: :Filespec)
+        spec.path = name
+        spec[:Desc] = description if description
+        spec.embed(file_or_io, name: name, register: true) if embed || !file_or_io.kind_of?(String)
+        spec
+      end
+
+      # :call-seq:
+      #   files.each_file(search: false) {|file_spec| block }   -> files
+      #   files.each_file(search: false)                        -> Enumerator
+      #
+      # Iterates over indirect file specification dictionaries of the PDF.
+      #
+      # By default, only the file specifications in their standard locations, namely in the
+      # EmbeddedFiles name tree and in the page annotations, are returned. If the +search+ option is
+      # +true+, then all indirect objects are searched for file specification dictionaries which is
+      # much slower.
+      def each_file(search: false)
+        return to_enum(__method__, search: search) unless block_given?
+
+        if search
+          @document.each(current: false) do |obj|
+            yield(obj) if obj.type == :Filespec
+          end
+        else
+          seen = {}
+          tree = @document.catalog[:Names] && @document.catalog[:Names][:EmbeddedFiles]
+          tree.each_entry do |_, spec|
+            seen[spec] = true
+            yield(spec)
+          end if tree
+
+          @document.pages.each_page do |page|
+            next unless page[:Annots]
+            page[:Annots].each do |annot|
+              annot = @document.deref(annot)
+              next unless annot[:Subtype] == :FileAttachment
+              spec = @document.deref(annot[:FS])
+              yield(spec) unless seen.key?(spec)
+              seen[spec] = true
+            end
+          end
+        end
+
+        self
+      end
+
+    end
+
     include Images
+    include Files
 
     # Creates a new DocumentUtils object for the given PDF document.
     def initialize(document)
--- test/hexapdf/test_document_utils.rb
+++ test/hexapdf/test_document_utils.rb
@@ -2,6 +2,7 @@
 
 require 'test_helper'
 require 'stringio'
+require 'tempfile'
 require 'hexapdf/document'
 
 describe HexaPDF::DocumentUtils::Images do
@@ -72,6 +73,72 @@ describe HexaPDF::DocumentUtils::Images do
       images << @doc.add(Subtype: :Image, Mask: @doc.add(Subtype: :Image))
       images << @doc.add(Subtype: :Image, SMask: @doc.add(Subtype: :Image))
       assert_equal(images.sort, @doc.utils.each_image.to_a.sort)
+    end
+  end
+end
+
+
+describe HexaPDF::DocumentUtils::Files do
+  before do
+    @doc = HexaPDF::Document.new
+    @data = "embed-test"
+    @file = Tempfile.new('file-embed-test')
+    @file.write(@data)
+    @file.close
+  end
+
+  after do
+    @file.unlink
+  end
+
+  describe "add_file" do
+    it "adds a file using a filename and embeds it" do
+      spec = @doc.utils.add_file(@file.path)
+      assert_equal(File.basename(@file.path), spec.path)
+      assert_equal(@data, spec.embedded_file_stream.stream)
+    end
+
+    it "adds a reference to a file" do
+      spec = @doc.utils.add_file(@file.path, embed: false)
+      assert_equal(File.basename(@file.path), spec.path)
+      refute(spec.embedded_file?)
+    end
+
+    it "adds a file using an IO" do
+      @file.open
+      spec = @doc.utils.add_file(@file, name: 'test', embed: false)
+      assert_equal('test', spec.path)
+      assert_equal(@data, spec.embedded_file_stream.stream)
+    end
+
+    it "optionally sets the description of the file" do
+      spec = @doc.utils.add_file(@file.path, description: 'Some file')
+      assert_equal('Some file', spec[:Desc])
+    end
+
+    it "requires the name argument when given an IO object" do
+      assert_raises(ArgumentError) { @doc.utils.add_file(StringIO.new) }
+    end
+  end
+
+  describe "each_file" do
+    it "iterates only over named embedded files and file annotations if search=false" do
+      @doc.add(Type: :Filespec)
+      spec1 = @doc.utils.add_file(__FILE__)
+      spec2 = @doc.add(Type: :Filespec)
+      @doc.pages.add_page[:Annots] = [
+        {Subtype: :FileAttachment, FS: HexaPDF::Reference.new(spec1.oid, spec1.gen)},
+        {Subtype: :FileAttachment, FS: spec2},
+        {},
+      ]
+      assert_equal([spec1, spec2], @doc.utils.each_file.to_a)
+    end
+
+    it "iterates over all file specifications of the document if search=true" do
+      specs = []
+      specs << @doc.add(Type: :Filespec)
+      specs << @doc.add(Type: :Filespec)
+      assert_equal(specs, @doc.utils.each_file(search: true).to_a)
     end
   end
 end
