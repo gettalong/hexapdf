0b2b1eb0762820454ee98852cee85d5822e8e223
Thomas Leitner <t_leitner@gmx.at>
2016-10-09 00:23:19 +0200

    Document font wrapper interface

--- lib/hexapdf/font_loader.rb
+++ lib/hexapdf/font_loader.rb
@@ -8,7 +8,7 @@ module HexaPDF
   # the font loader doesn't have the requested font, it has to return +nil+.
   #
   # The returned object has to be a PDF font wrapper and not the generic font object because it
-  # needs to be usable by the PDF canvas.
+  # needs to be usable by the PDF canvas. See below for details.
   #
   #
   # == Implementation of a Font Loader
@@ -24,6 +24,25 @@ module HexaPDF
   # following common options:
   #
   # variant:: The font variant that should be used (e.g. :none, :bold, :italic, :bold_italic).
+  #
+  #
+  # == Font Wrappers
+  #
+  # A font wrapper needs to provide the following generic interface so that it can be used correctly
+  # by HexaPDF:
+  #
+  # dict::
+  #     This method needs to return the PDF font dictionary that represents the wrapped font.
+  #
+  # decode_utf8(str)::
+  #     This method needs to convert the given string into an array of glyph objects. The glyph
+  #     objects themselves are treated as opaque objects by HexaPDF::Content::Canvas.
+  #
+  # encode(glyph)::
+  #     This method takes a single glyph object, that needs to be compatible with the font wrapper,
+  #     and returns an encoded string that can be decoded with the font dictionary returned by
+  #     \#dict.
+  #
   module FontLoader
 
     autoload(:Standard14, 'hexapdf/font_loader/standard14')
