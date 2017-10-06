85e4a37ddfc77fb6ca09152dc04ed4fce3ad1d48
Thomas Leitner <t_leitner@gmx.at>
2017-10-06 17:37:34 +0200

    Update text layouter styling example to showcase superscript styling

--- examples/text_layouter_styling.rb
+++ examples/text_layouter_styling.rb
@@ -51,18 +51,20 @@ body = Style.new(font: doc.fonts.load("Times"),
                  font_size: 10, align: :justify,
                  text_indent: 20)
 body.line_spacing(:fixed, 16)
-highlight = Style.new(font: doc.fonts.load("Times", variant: :bold_italic),
-                      text_rendering_mode: :stroke,
-                      stroke_color: [255, 0, 0], stroke_width: 0.2,
-                      stroke_join_style: :round)
-highlight.underlay_callback do |canv, box|
+highlight1 = Style.new(font: doc.fonts.load("Times", variant: :bold_italic),
+                       text_rendering_mode: :stroke,
+                       stroke_color: [255, 0, 0], stroke_width: 0.2,
+                       stroke_join_style: :round)
+highlight2 = Style.new(font: doc.fonts.load("Times", variant: :italic))
+highlight2.underlay_callback do |canv, box|
   canv.fill_color(240, 240, 0).opacity(fill_alpha: 0.5).
     rectangle(0, 0, box.width, box.height).fill
 end
-highlight.overlay_callback do |canv, box|
+highlight2.overlay_callback do |canv, box|
   canv.line_width(1).stroke_color([0, 255, 0]).
     line(0, 0, box.width, box.height).stroke
 end
+highlight3 = Style.new(font: doc.fonts.load("Times"), superscript: true)
 intro = Style.new(font: doc.fonts.load("Times", variant: :bold),
                   fill_color: [0, 0, 160], font_size: 14)
 
@@ -79,10 +81,13 @@ layouter = TextLayouter.new(items: [fragment(sample_text, body)],
                             width: width, style: body)
 y_base = draw_text(layouter, canvas, left, y_base - 20)
 
+highlights = [highlight1, highlight2, highlight3]
+highlight = nil
 items = sample_text.split(/(Lorem ipsum dolor sit|\b\w{2,4}\b)/).map do |str|
   if str.length >= 2 && str.length <= 4
     fragment(str, highlight)
   elsif str =~ /Lorem/
+    highlight = highlights.shift
     fragment(str, intro)
   else
     fragment(str, body)
