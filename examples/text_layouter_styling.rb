ff52ca799d32d539c4780ded318f8b433e30c876
Thomas Leitner <t_leitner@gmx.at>
2017-10-09 20:39:20 +0200

    Update text layouter styling example to better show possibilites

--- examples/text_layouter_styling.rb
+++ examples/text_layouter_styling.rb
@@ -17,13 +17,20 @@ require 'hexapdf'
 
 include HexaPDF::Layout
 
+sample_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+in voluptate velit esse cillum dolore eu fugiat nulla pariatur.".tr("\n", ' ')
+
 # Wraps the text in a TextFragment using the given style.
 def fragment(text, style)
-  TextFragment.new(items: style.font.decode_utf8(text),
-                                    style: style)
+  style = Style.new(style)
+  TextFragment.new(items: style.font.decode_utf8(text), style: style)
 end
 
-# Draws the text at the given [x, y] position onto the canvas.
+# Draws the text at the given [x, y] position onto the canvas and returns the
+# new y position.
 def draw_text(layouter, canvas, x, y)
   rest, height = layouter.fit
   raise "Error" unless rest.empty?
@@ -31,69 +38,71 @@ def draw_text(layouter, canvas, x, y)
   y - height
 end
 
-sample_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-officia deserunt mollit anim id est laborum.\u{2029}".tr("\n", ' ') * 3
-
 doc = HexaPDF::Document.new
 
-heading = Style.new(font: doc.fonts.add("Helvetica", variant: :bold),
-                    font_size: 32, align: :center,
-                    fill_color: [200], text_rendering_mode: :fill_stroke,
-                    stroke_color: [255, 0, 0], stroke_alpha: 0.7,
-                    stroke_width: 1, stroke_dash_pattern: [0.5, 1, 1.5],
-                    stroke_cap_style: :round, stroke_join_style: :round)
-body = Style.new(font: doc.fonts.add("Times"),
-                 font_size: 10, align: :justify,
-                 text_indent: 20)
-body.line_spacing(:fixed, 16)
-highlight1 = Style.new(font: doc.fonts.add("Times", variant: :bold_italic),
-                       text_rendering_mode: :stroke,
-                       stroke_color: [255, 0, 0], stroke_width: 0.2,
-                       stroke_join_style: :round)
-highlight2 = Style.new(font: doc.fonts.add("Times", variant: :italic))
-highlight2.underlay_callback do |canv, box|
-  canv.fill_color(240, 240, 0).opacity(fill_alpha: 0.5).
-    rectangle(0, 0, box.width, box.height).fill
-end
-highlight2.overlay_callback do |canv, box|
-  canv.line_width(1).stroke_color([0, 255, 0]).
-    line(0, 0, box.width, box.height).stroke
-end
-highlight3 = Style.new(font: doc.fonts.add("Times"), superscript: true)
-intro = Style.new(font: doc.fonts.add("Times", variant: :bold),
-                  fill_color: [0, 0, 160], font_size: 14)
+base_font = doc.fonts.add("Times")
+base_style = {font: base_font, font_size: 15, text_indent: 20}
+styles = {
+  "Fonts | Font Sizes | Colors" => [
+    {font: doc.fonts.add("Times", variant: :italic),
+     font_size: 12, fill_color: [0, 0, 255]},
+    {font: doc.fonts.add("Courier"), font_size: 14,
+     fill_color: [0, 255, 0]},
+    {font: doc.fonts.add("Helvetica", variant: :bold),
+     font_size: 20, fill_alpha: 0.5},
+  ],
+  "Character Spacing | Word Spacing | Horizontal Scaling" => [
+    {**base_style, character_spacing: 3},
+    {**base_style, horizontal_scaling: 150},
+    {**base_style, word_spacing: 15},
+  ],
+  "Text Rise" => [
+    {**base_style, text_rise: 5},
+    {**base_style, text_rise: -3},
+  ],
+  "Subscript | Superscript" => [
+    {**base_style, font_size: 15, subscript: true},
+    {**base_style, font_size: 15, superscript: true},
+  ],
+  "Text Rendering Mode" => [
+    {**base_style, text_rendering_mode: :stroke,
+     stroke_width: 0.1},
+    {**base_style, font_size: 20, text_rendering_mode: :fill_stroke,
+     stroke_color: [0, 255, 0], stroke_width: 0.7,
+     stroke_dash_pattern: [0.5, 1, 1.5], stroke_cap_style: :round},
+  ],
+  "Underlays | Overlays" => [
+    {**base_style, underlay_callback: lambda do |canv, box|
+       canv.fill_color(240, 240, 0).opacity(fill_alpha: 0.5).
+         rectangle(0, 0, box.width, box.height).fill
+      end},
+    {**base_style, overlay_callback: lambda do |canv, box|
+       canv.line_width(1).stroke_color([0, 255, 0]).
+         line(0, -box.y_min, box.width, box.y_max - box.y_min).stroke
+      end},
+  ],
+}
 
 canvas = doc.pages.add.canvas
-y_base = 800
+y = 800
 left = 50
 width = 500
 
-layouter = TextLayouter.new(items: [fragment("This is a header", heading)],
-                            width: width, style: heading)
-y_base = draw_text(layouter, canvas, left, y_base)
-
-layouter = TextLayouter.new(items: [fragment(sample_text, body)],
-                            width: width, style: body)
-y_base = draw_text(layouter, canvas, left, y_base - 20)
-
-highlights = [highlight1, highlight2, highlight3]
-highlight = nil
-items = sample_text.split(/(Lorem ipsum dolor sit|\b\w{2,4}\b)/).map do |str|
-  if str.length >= 2 && str.length <= 4
-    fragment(str, highlight)
-  elsif str =~ /Lorem/
-    highlight = highlights.shift
-    fragment(str, intro)
-  else
-    fragment(str, body)
+styles.each do |desc, variations|
+  items = sample_text.split(/(Lorem ipsum dolor|\b\w{2,5}\b)/).map do |str|
+    if str.length >= 3 && str.length <= 5
+      fragment(str, variations[str.length % variations.length])
+    elsif str.length == 2
+      fragment(str, variations.first)
+    elsif str =~ /Lorem/
+      fragment(str, variations.last)
+    else
+      fragment(str, base_style)
+    end
   end
+  items.unshift(fragment(desc + ": ", fill_color: [255, 0, 0], **base_style))
+  layouter = TextLayouter.new(items: items, width: width, style: base_style)
+  y = draw_text(layouter, canvas, left, y) - 20
 end
 
-layouter = TextLayouter.new(items: items, width: width, style: body)
-draw_text(layouter, canvas, left, y_base - 20)
 doc.write("text_layouter_styling.pdf", optimize: true)
