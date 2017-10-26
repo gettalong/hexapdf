c835e13b7fd02d9cbe134db24cff904a54befcad
Thomas Leitner <t_leitner@gmx.at>
2017-10-26 14:41:13 +0200

    Add test file that loads all PDF type files
    The PDF type files are autoloaded but since some files don't have a
    corresponding test file they aren't effectively loaded. Fix this by
    force-loading them so that at least syntax errors are checked for.

---
+++ test/hexapdf/test_type.rb
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+
+require 'test_helper'
+require 'hexapdf/type'
+
+describe HexaPDF::Type do
+  it "all autoload files have no syntax error" do
+    HexaPDF::Type.constants.each do |const|
+      HexaPDF::Type.const_get(const) # no assert needed here
+    end
+    HexaPDF::Type::Actions.constants.each do |const|
+      HexaPDF::Type::Actions.const_get(const) # no assert needed here
+    end
+  end
+end
