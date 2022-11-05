412b3619a76a6feba996a2738f20aa50105308d9
Thomas Leitner <t_leitner@gmx.at>
2022-11-05 07:27:35 +0100

    Support verifying document timestamp signatures

--- test/hexapdf/type/signature/common.rb
+++ test/hexapdf/type/signature/common.rb
@@ -65,6 +65,32 @@ module HexaPDF
           end
       end
 
+      def timestamp_certificate
+        @timestamp_certificate ||=
+          begin
+            name = OpenSSL::X509::Name.parse('/CN=timestamp/DC=gettalong')
+
+            signer_cert = OpenSSL::X509::Certificate.new
+            signer_cert.serial = 3
+            signer_cert.version = 2
+            signer_cert.not_before = Time.now - 86400
+            signer_cert.not_after = Time.now + 86400
+            signer_cert.public_key = signer_key.public_key
+            signer_cert.subject = name
+            signer_cert.issuer = ca_certificate.subject
+
+            extension_factory = OpenSSL::X509::ExtensionFactory.new
+            extension_factory.subject_certificate = signer_cert
+            extension_factory.issuer_certificate = ca_certificate
+            signer_cert.add_extension(extension_factory.create_extension('subjectKeyIdentifier', 'hash'))
+            signer_cert.add_extension(extension_factory.create_extension('basicConstraints', 'CA:FALSE'))
+            signer_cert.add_extension(extension_factory.create_extension('keyUsage', 'digitalSignature'))
+            signer_cert.add_extension(extension_factory.create_extension('extendedKeyUsage', 'timeStamping', true))
+            signer_cert.sign(ca_key, OpenSSL::Digest.new('SHA1'))
+
+            signer_cert
+          end
+      end
     end
 
   end
