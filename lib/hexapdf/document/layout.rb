d6866ae3c2dcaa8ab8cefa03b68d3878ace90c18
Thomas Leitner <t_leitner@gmx.at>
2023-05-18 14:18:06 +0200

    Remove unnecessary space characters

--- lib/hexapdf/document/layout.rb
+++ lib/hexapdf/document/layout.rb
@@ -410,9 +410,9 @@ module HexaPDF
         "Lorem ipsum dolor sit amet, con\u{00AD}sectetur adipis\u{00AD}cing elit, sed " \
           "do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "Ut enim ad minim veniam, quis nostrud exer\u{00AD}citation ullamco laboris nisi ut " \
-          "aliquip ex ea commodo consequat. ",
+          "aliquip ex ea commodo consequat.",
         "Duis aute irure dolor in reprehen\u{00AD}derit in voluptate velit esse cillum dolore " \
-          "eu fugiat nulla pariatur. ",
+          "eu fugiat nulla pariatur.",
         "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt " \
           "mollit anim id est laborum.",
       ]
