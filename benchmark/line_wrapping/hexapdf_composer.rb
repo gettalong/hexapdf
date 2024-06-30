c093029e99b873513257e307d0ee712bd082a5a5
Thomas Leitner <t_leitner@gmx.at>
2024-06-30 22:24:46 +0200

    Update line wrapping benchmark script for HexaPDF composer
    It seems that the middle ground works best for HexaPDF: Line by line is
    slower but saves memory. Using all lines at once is faster but
    takes (much) more memory. Processing the file in bigger chunks (of e.g.
    1000 lines) is still fast and uses much less memory, especially in the
    width=50 case.

--- benchmark/line_wrapping/hexapdf_composer.rb
+++ benchmark/line_wrapping/hexapdf_composer.rb
@@ -7,8 +7,12 @@ file = ARGV[0]
 width = ARGV[1].to_i
 height = 1000
 
+lines_per_chunk = 1000
+data = File.readlines(file)
 HexaPDF::Composer.create(ARGV[2], page_size: [0, 0, width, height], margin: 0) do |pdf|
-  pdf.text(File.read(file), font_features: {kern: false},
+  (data.size / lines_per_chunk + 1).times do |i|
+    pdf.text(data[i * lines_per_chunk, lines_per_chunk].join, font_features: {kern: false},
            font: ARGV[3] || "Times", font_size: 10, last_line_gap: true,
            line_spacing: {type: :fixed, value: 11.16})
+  end
 end
